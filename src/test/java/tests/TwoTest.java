3a6fb850eb431da0913933e0562fa8145642f534
GaneshMudiraj <121211302+GaneshMudiraj@users.noreply.github.com>
2023-04-03 10:01:57 +0530

    Updated by Vamshi

--- src/test/java/tests/TwoTest.java
+++ src/test/java/tests/TwoTest.java
@@ -14,6 +14,8 @@ public class TwoTest extends Base{
 	@Test
 	public void testTwo() throws IOException, InterruptedException {
 		
+		System.out.println("Vamshi has updated this code");
+		
 		System.out.println(" TestTwo");
 		
 		 driver = intializeDriver();
